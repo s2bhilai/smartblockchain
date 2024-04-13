de718f2738c8b1158ae6245a09a33d7503b7aabf
Subin <subinbs4a@gmail.com>
2024-04-13 09:27:28 +0300

    Added Trie

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -25,7 +25,11 @@ describe("Block", () => {
 
     beforeEach(() => {
       lastBlock = Block.genesis();
-      minedBlock = Block.mineBlock({ lastBlock, beneficiary: "beneficiary" });
+      minedBlock = Block.mineBlock({
+        lastBlock,
+        beneficiary: "beneficiary",
+        transactionSeries: [],
+      });
     });
 
     it("mines a block", () => {
@@ -79,7 +83,11 @@ describe("Block", () => {
 
     beforeEach(() => {
       lastBlock = Block.genesis();
-      block = Block.mineBlock({ lastBlock, beneficiary: "beneficiary" });
+      block = Block.mineBlock({
+        lastBlock,
+        beneficiary: "beneficiary",
+        transactionSeries: [],
+      });
     });
 
     it("resolves when the block is genesis block", () => {
---
+++ store/trie.test.js
@@ -0,0 +1,45 @@
+const Trie = require("./trie");
+
+describe("Trie", () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  it("has a rootHash", () => {
+    expect(trie.rootHash).not.toBe(undefined);
+  });
+
+  describe("put()", () => {
+    it("stores a value under a key", () => {
+      const key = "foo";
+      const value = "bar";
+      trie.put({ key, value });
+
+      expect(trie.get({ key })).toEqual(value);
+    });
+
+    it("generates a new root hash after entering the value", () => {
+      const originalRootHash = trie.rootHash;
+
+      trie.put({ key: "foo", value: "bar" });
+      expect(trie.rootHash).not.toEqual(originalRootHash);
+    });
+  });
+
+  describe("get()", () => {
+    it("returns a copy of stored value", () => {
+      const key = "foo";
+      const value = { one: 1 };
+      trie.put({ key, value });
+      const gottenValue = trie.get({ key });
+
+      value.one = 2;
+      const gottenValue2 = trie.get({ key });
+      console.log("gottenValue2:", gottenValue2);
+
+      expect(gottenValue).toEqual({ one: 1 });
+    });
+  });
+});
